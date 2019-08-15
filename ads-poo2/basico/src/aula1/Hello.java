5791dd8fe8333b6285201b5c2410de26b1445a7c
antoniorcn <antoniorcn@hotmail.com>
2019-08-14 22:29:53 -0300

    LES Aula 2 parte 3

--- ads-poo2/basico/src/aula1/Hello.java
+++
@@ -1,9 +0,0 @@
-package aula1;
-
-public class Hello {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		main(args);
-	}
-}
--- ads-poo2/basico/src/aula2/ArCondicionado.java
+++
@@ -1,9 +0,0 @@
-package aula2;
-
-public class ArCondicionado {
-	String marca;
-	String modelo;
-	int numeroSerie;
-	float tamanho;
-	String cor;
-}
