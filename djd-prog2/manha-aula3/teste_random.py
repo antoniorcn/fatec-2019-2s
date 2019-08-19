d53952986a2566ae1b797b54df12c06ec2dd3bfe
antoniorcn <antoniorcn@hotmail.com>
2019-08-19 12:34:58 -0300

    Create teste_random.py
    .

---
+++ djd-prog2/manha-aula3/teste_random.py
@@ -0,0 +1,7 @@
+from random import random
+num = input("Digite um numero entre 1 e 6")
+a = random()
+print("Numero Aleatorio:", a)
+numero = round(a * 5.0) + 1
+print("Voce escolheu o numero: ", num)
+print("O dado sorteou o numero: ", numero)
