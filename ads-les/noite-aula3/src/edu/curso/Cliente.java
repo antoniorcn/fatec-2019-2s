4e5202d873d36f779ba9ec1ff54a6244b9a3b3fc
antoniorcn <antoniorcn@hotmail.com>
2019-08-21 21:47:14 -0300

    ADS Noite Aula 3 parte 4

---
+++ ads-les/noite-aula3/src/edu/curso/Cliente.java
@@ -0,0 +1,31 @@
+package edu.curso;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		try(Socket serv = new Socket("127.0.0.1", 15678)) {
+			InputStream in = serv.getInputStream();
+			OutputStream out = serv.getOutputStream();
+			boolean sair = false;
+			while(!sair) { 
+				if (in.available() > 0) { 
+					int i = in.read();
+					System.out.print((char)i);
+				}
+				if (System.in.available() > 0) { 
+					int i = System.in.read();
+					out.write(i);
+					out.flush();
+				}				
+			}
+		} catch (Exception e) { 
+			e.printStackTrace();
+		}
+
+	}
+
+}
