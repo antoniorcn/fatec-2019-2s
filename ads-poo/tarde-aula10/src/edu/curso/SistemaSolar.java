1af19a25dd95700a8ae4477c878d6169eee778af
antoniorcn <antoniorcn@hotmail.com>
2019-10-10 20:08:45 -0300

    Bolinha

--- ads-poo/tarde-aula10/src/edu/curso/SistemaSolar.java
+++ ads-poo/tarde-aula10/src/edu/curso/SistemaSolar.java
@@ -7,70 +7,64 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class SistemaSolar extends Application {
-	
-//	public void start(Stage stage) { 
-//		Group grp = new Group();
-//		Canvas canvas = new Canvas( 512 - 64, 256 );
-//		Scene scn = new Scene(grp, 512, 512);
-//		grp.getChildren().add(canvas);
-//		
-//		Image img = new Image("file://C:/tmp/images/space.png");
-//		GraphicsContext gc = 
-//				canvas.getGraphicsContext2D();
-//		
-//		new AnimationTimer() {
-//			@Override
-//			public void handle(long now) {
-//				gc.clearRect(0,  0, 448, 256);
-//				gc.drawImage(img, 0,  0);
-//			} 
-//		}.start();
-//		// gc.rect(100, 100, 200, 200);
-//		
-//		
-//		stage.setScene(scn);
-//		stage.setTitle("Sistema Solar");
-//		stage.show();
-//	}
-	
-	public void start(Stage theStage) 
-	{
-	    theStage.setTitle( "Canvas Example" );
-	        
-	    Group root = new Group();
-	    Scene theScene = new Scene( root );
-	    theStage.setScene( theScene );
-	    Canvas canvas = new Canvas( 400, 200 );
-	    root.getChildren().add( canvas );
-	    GraphicsContext gc = canvas.getGraphicsContext2D();
-	    gc.setFill( Color.RED );
-	    gc.setStroke( Color.BLACK );
-	    gc.setLineWidth(2);
-	    gc.fillRect(100, 100, 200, 200);
-
-	    gc.setFill( Color.YELLOW );
-	    gc.setStroke( Color.BLUE );
-	    gc.beginPath();
-	    gc.moveTo(200, 200);
-	    gc.lineTo(100, 100);
-	    gc.arc(100, 100, 50, 50, 0, 360);
-	    gc.stroke();
-	    // Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
-	    // gc.setFont( theFont );
-	    // gc.fillText( "Hello, World!", 60, 50 );
-	    // gc.strokeText( "Hello, World!", 60, 50 );	    
-	    theStage.show();
-	}
-
 	public static void main(String[] args) {
 		SistemaSolar.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Group grp = new Group();
+		Scene scn = new Scene(grp);
+		Image space = new Image(getClass().getResourceAsStream("/images/space.png"));
+		Canvas canvas = new Canvas (space.getWidth(), space.getHeight());
+		grp.getChildren().add(canvas);
+		GraphicsContext ctx = canvas.getGraphicsContext2D();
+		
+		Image earth = new Image(getClass().getResourceAsStream("/images/earth.png"));
+		Image sun = new Image(getClass().getResourceAsStream("/images/sun.png"));
+		Image mars = new Image(getClass().getResourceAsStream("/images/mars.png"));
+		
+		long nanoTime = System.nanoTime();
+		
+		new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+				double time = nanoTime - now / 10000000.0;
+				double time2 = nanoTime - now / 10000000.0 * 1.5;
+				double centerx = canvas.getWidth() / 2;
+				double centery = canvas.getHeight() / 2;
+				double angle = time % 360;
+				double angle2 = time2 % 360;
+				double angleRad = angle / 180 * Math.PI;
+				double angleRad2 = angle2 / 180 * Math.PI;
+				double raio = 100.0;
+				double earthCornerX = centerx - earth.getWidth() / 2;
+				double earthCornerY = centery - earth.getHeight() / 2;
+				double earthx = earthCornerX + raio * Math.sin(angleRad);
+				double earthy = earthCornerY + raio * Math.cos(angleRad);
+				double sunx = centerx - sun.getWidth() / 2;
+				double suny = centery - sun.getHeight() / 2;
+				
+				double marsCornerX = centerx - mars.getWidth() / 2;
+				double marsCornerY = centery - mars.getHeight() / 2;
+				double marsx = marsCornerX + 2 * raio * Math.sin(angleRad2);
+				double marsy = marsCornerY + 2 * raio * Math.cos(angleRad2);
+
+				
+				ctx.drawImage(space,  0, 0);
+				ctx.drawImage(sun,  sunx, suny);
+				ctx.drawImage(earth,  earthx, earthy);
+				ctx.drawImage(mars,  marsx, marsy);
+			} 
+		}.start();
+		
+		stage.setTitle("Sistema Solar");
+		stage.setScene(scn);
+		stage.show();
+		
 	}
 
 }
