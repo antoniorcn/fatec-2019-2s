6214ab04b1d4d77176499b5a85d593f733fff44d
antoniorcn <antoniorcn@hotmail.com>
2019-10-16 20:47:37 -0300

    Aula 11 noite parte 1

---
+++ ads-les/noite-aula11-parte1/WebContent/WEB-INF/jsp/pagina1.jsp
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		<h1> Teste da pagina 1</h1>
+		<h2>Mensagem: ${MENSAGEM}</h2>
+		<h3>Data e Hora: ${DATAATUAL}</h2>
+	</body>
+</html>
---
+++ ads-les/noite-aula11-parte1/src/edu/curso/ApplicationInitializer.java
@@ -0,0 +1,23 @@
+package edu.curso;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+public class ApplicationInitializer implements WebApplicationInitializer{
+	@Override
+	public void onStartup(ServletContext ctx) throws ServletException {
+		AnnotationConfigWebApplicationContext appCtx =
+				new AnnotationConfigWebApplicationContext();
+		appCtx.setServletContext(ctx);
+		appCtx.register(Configuracao.class);
+		
+		Dynamic dispatcher = ctx.addServlet("dispatcher", 
+				new DispatcherServlet(appCtx));
+		dispatcher.addMapping("/");
+		dispatcher.setLoadOnStartup(1);		
+	}
+}
