c6929ee3086d19e75207aa67213d11deed38a687
antoniorcn <antoniorcn@hotmail.com>
2019-11-11 11:10:47 -0300

    Prog2 Manha Aula 14 parte 2

--- djd-prog2/manha-aula14/pulo-sprite.py
+++ djd-prog2/manha-aula14/pulo-sprite.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 class Personagem(pygame.sprite.Sprite):
@@ -6,21 +7,27 @@ class Personagem(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         img = pygame.image.load("capitao_nasc.png").convert_alpha()
         size = (122, 297)
+        self.hp = 100
         self.y_vel = 0
         self.x_vel = 0
         self.image = pygame.transform.scale(img, size)
         self.rect = pygame.Rect((0, 0), size)
 
     def update(self, *args):
-        capitao.y_vel += args[0]
-        capitao.rect.y += (capitao.y_vel / args[1])
-        capitao.rect.x += self.x_vel
+        self.y_vel += args[0]
+        self.rect.y += (self.y_vel / args[1])
+        self.rect.x += self.x_vel
 
     def processa_eventos(self, eventos):
+        global tiros
         for e in eventos:
             if e.type == pygame.KEYDOWN:
                 if e.key == pygame.K_SPACE:
                     self.y_vel = -300
+                if e.key == pygame.K_RETURN:
+                    tiro = Tiro()
+                    tiro.rect.topleft = self.rect.midright
+                    tiros.add(tiro)
                 elif e.key == pygame.K_RIGHT:
                     self.x_vel = 5
                 elif e.key == pygame.K_LEFT:
@@ -33,8 +40,38 @@ class Piso(pygame.sprite.Sprite):
         img = pygame.image.load("piso.png").convert_alpha()
         size = (800, 131)
         self.image = pygame.transform.scale(img, size)
-        self.rect = img.get_rect()
+        self.rect = pygame.Rect((0, 0), size)
         self.rect.y = 500
+
+
+class Inimigo(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load("coelho.png").convert_alpha()
+        size = (51, 110)
+        self.y_vel = 0
+        self.x_vel = 0
+        self.image = pygame.transform.scale(img, size)
+        self.rect = pygame.Rect((0, 0), size)
+
+    def update(self, *args):
+        self.y_vel += args[0]
+        self.rect.y += (self.y_vel / args[1])
+        self.rect.x += self.x_vel
+
+
+class Tiro(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load("projetil.png").convert_alpha()
+        size = (32, 14)
+        img_scaled = pygame.transform.scale(img, size)
+        self.image = pygame.transform.flip(img_scaled, True, False)
+        self.rect = pygame.Rect((0, 0), size)
+        self.x_vel = 20
+
+    def update(self, *args):
+        self.rect.x += self.x_vel
 
 
 PRETO = (0, 0, 0)
@@ -55,10 +92,25 @@ herois.add(capitao)
 plataformas = pygame.sprite.Group()
 plataformas.add(piso)
 
+inimigos = pygame.sprite.Group()
+
+tiros = pygame.sprite.Group()
+
+def criar_coelho():
+    inimigo = Inimigo()
+    inimigo.rect.x = random.randint(400, 800)
+    inimigo.rect.y = random.randint(-200, 0)
+    inimigos.add(inimigo)
+
+criar_coelho()
+criar_coelho()
 
 while True:
     # Calcular Regras
     herois.update(gravidade, fator)
+    inimigos.update(gravidade, fator)
+    tiros.update(gravidade, fator)
+    barra_x = 200 * capitao.hp / 100
 
     if pygame.sprite.spritecollide(capitao, plataformas, False):
         capitao.y_vel = 0
@@ -66,10 +118,27 @@ while True:
         #    if pygame.sprite.collide_mask(capitao, p):
         #        capitao.y_vel = 0
 
+    if pygame.sprite.groupcollide(inimigos, plataformas, True, False):
+        capitao.hp -= 1
+        criar_coelho()
+
+    if pygame.sprite.groupcollide(tiros, inimigos, True, True):
+        capitao.hp += 1
+        criar_coelho()
+
+    for t in tiros:
+        if t.rect.x > 800:
+            t.kill()
+
     # Desenhar
     tela.fill(PRETO)
     herois.draw(tela)
+    tiros.draw(tela)
     plataformas.draw(tela)
+    pygame.draw.rect(tela, VERMELHO, piso.rect, 5)
+    inimigos.draw(tela)
+    pygame.draw.rect(tela, VERMELHO, pygame.Rect((20, 20), (barra_x, 20)), 0)
+    pygame.draw.rect(tela, AMARELO, pygame.Rect((20, 20), (200, 20)), 5)
     pygame.display.update()
 
     pygame.time.delay(50)
