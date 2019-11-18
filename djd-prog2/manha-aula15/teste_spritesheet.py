1926cda4f0cc0c0e583a7d0ca498c015f96c30a5
antoniorcn <antoniorcn@hotmail.com>
2019-11-18 11:47:56 -0300

    Aula prog2 manha parte 3

--- djd-prog2/manha-aula15/teste_spritesheet.py
+++ djd-prog2/manha-aula15/teste_spritesheet.py
@@ -6,29 +6,35 @@ PRETO = (0, 0, 0)
 
 class Heroi(pygame.sprite.Sprite):
     def __init__(self):
+        self.estado = "CAMINHANDO_DIREITA"
         pygame.sprite.Sprite.__init__(self)
         self.spritesheet = pygame.image.load("EpicArmor.png").convert_alpha()
         self.width = 64
         self.height = 64
         self.colunas = 9
-        self.animacao = [28, 29, 30, 31, 32, 33, 34]
+        self.animacoes = {
+            "PARADO_DIREITA": [27],
+            "CAMINHANDO_DIREITA": [28, 29, 30, 31, 32, 33, 34]
+        }
         self.image = pygame.Surface((self.width, self.height), 0)
         self.frame_index = 0
-        self.gId = self.animacao[self.frame_index]
+        self.gId = 0
         self.rect = pygame.Rect((100, 100), (self.width, self.height))
         self.vel_x = 0
         self.vel_y = 0
 
     def update(self):
         self.rect.move_ip(self.vel_x, self.vel_y)
-        self.gId = self.animacao[self.frame_index]
+        movimentos = self.animacoes[self.estado]
+        self.frame_index += 1
+        if self.frame_index >= len(movimentos):
+            self.frame_index = 0
+        self.gId = movimentos[self.frame_index]
         col = self.gId % self.colunas
         lin = self.gId // self.colunas
         x = col * self.width
         y = lin * self.height
-        self.frame_index += 1
-        if self.frame_index >= len(self.animacao):
-            self.frame_index = 0
+
         self.image = self.spritesheet.subsurface(x, y, self.width, self.height)
 
     def processar_eventos(self, eventos):
@@ -36,6 +42,7 @@ class Heroi(pygame.sprite.Sprite):
             if e.type == pygame.KEYDOWN:
                 if e.key == pygame.K_RIGHT:
                     self.vel_x = 1
+                    self.estado = "CAMINHANDO_DIREITA"
                 elif e.key == pygame.K_LEFT:
                     self.vel_x = -1
                 elif e.key == pygame.K_UP:
@@ -43,8 +50,9 @@ class Heroi(pygame.sprite.Sprite):
                 elif e.key == pygame.K_DOWN:
                     self.vel_y = 1
             elif e.type == pygame.KEYUP:
-                if e.key in [pygame.K_RIGHT, pygame.K_LEFT]:
+                if e.key == pygame.K_RIGHT:
                     self.vel_x = 0
+                    self.estado = "PARADO_DIREITA"
                 elif e.key in [pygame.K_UP, pygame.K_DOWN]:
                     self.vel_y = 0
 
